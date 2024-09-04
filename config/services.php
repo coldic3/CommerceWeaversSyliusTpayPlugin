2c94cea5e5234bd2a290264067115f3ec7be7ee3
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2024-09-04 14:00:52 +0200

    Add Tpay as a payment method to default's fixtures

--- config/services.php
+++ config/services.php
@@ -9,6 +9,8 @@ return function(ContainerConfigurator $container): void {
 
     $parameters = $container->parameters();
     $parameters
+        ->set('env(TPAY_CLIENT_ID)', '')
+        ->set('env(TPAY_CLIENT_SECRET)', '')
         ->set('commerce_weavers_tpay.payum.create_transaction.success_route', 'sylius_shop_order_thank_you')
         ->set('commerce_weavers_tpay.payum.create_transaction.error_route', 'sylius_shop_order_thank_you')
         ->set('commerce_weavers_tpay.payum.create_transaction.notify_route', 'commerce_weavers_tpay_payment_notification')
