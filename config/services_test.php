da36838f062508b72cb5a0894c7e5f6c12d38ec8
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2024-09-17 15:48:50 +0200

    Remove duplicated services directory import

--- config/services_test.php
+++ config/services_test.php
@@ -10,9 +10,8 @@ use CommerceWeavers\SyliusTpayPlugin\Test\Payum\Factory\TestTpayGatewayFactory;
 use Payum\Core\Bridge\Symfony\Builder\GatewayFactoryBuilder;
 
 return function(ContainerConfigurator $container): void {
-    $container->import('services/**/*.php');
-
     $services = $container->services();
+
     $services->set('commerce_weavers_sylius_tpay.payum.factory.tpay_gateway', GatewayFactoryBuilder::class)
         ->args([
             TestTpayGatewayFactory::class,
