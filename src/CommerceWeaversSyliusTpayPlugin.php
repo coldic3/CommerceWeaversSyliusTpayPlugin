35529b87671336f7cc07b8bc57f8dac9ee8d995b
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2024-09-19 16:25:59 +0200

    Add OrderShopUserItemExtension and OrderVisitorItemExtension decorators to exclude `shop_pay` operation from a cart filter

--- src/CommerceWeaversSyliusTpayPlugin.php
+++ src/CommerceWeaversSyliusTpayPlugin.php
@@ -4,19 +4,12 @@ declare(strict_types=1);
 
 namespace CommerceWeavers\SyliusTpayPlugin;
 
-use CommerceWeavers\SyliusTpayPlugin\DependencyInjection\CompilerPass\AddShopPayOperationToAllowedNonGetOperationsPass;
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class CommerceWeaversSyliusTpayPlugin extends Bundle
 {
     use SyliusPluginTrait;
-
-    public function build(ContainerBuilder $container): void
-    {
-        $container->addCompilerPass(new AddShopPayOperationToAllowedNonGetOperationsPass(), priority: -1024);
-    }
 
     public function getPath(): string
     {
