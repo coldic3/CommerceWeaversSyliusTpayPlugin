CommerceWeavers/SyliusTpayPlugin

Support Google Pay in API with PayByGooglePay command, handler and factory

config/services/api/command.php
@@ -7,6 +7,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 use CommerceWeavers\SyliusTpayPlugin\Api\Command\AbstractPayByHandler;
 use CommerceWeavers\SyliusTpayPlugin\Api\Command\PayByBlikHandler;
 use CommerceWeavers\SyliusTpayPlugin\Api\Command\PayByCardHandler;
+use CommerceWeavers\SyliusTpayPlugin\Api\Command\PayByGooglePayHandler;
 use CommerceWeavers\SyliusTpayPlugin\Api\Command\PayByRedirectHandler;
 use CommerceWeavers\SyliusTpayPlugin\Api\Command\PayHandler;
 
@@ -37,6 +38,11 @@ return function(ContainerConfigurator $container): void {
     ;
 
     $services->set('commerce_weavers_tpay.api.command.pay_by_card_handler', PayByCardHandler::class)
+        ->parent('commerce_weavers_tpay.api.command.abstract_pay_by_handler')
+        ->tag('messenger.message_handler')
+    ;
+
+    $services->set('commerce_weavers_tpay.api.command.pay_by_google_pay_handler', PayByGooglePayHandler::class)
         ->parent('commerce_weavers_tpay.api.command.abstract_pay_by_handler')
         ->tag('messenger.message_handler')
     ;

config/services/api/factory.php
@@ -6,6 +6,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use CommerceWeavers\SyliusTpayPlugin\Api\Factory\NextCommand\PayByBlikFactory;
 use CommerceWeavers\SyliusTpayPlugin\Api\Factory\NextCommand\PayByCardFactory;
+use CommerceWeavers\SyliusTpayPlugin\Api\Factory\NextCommand\PayByGooglePayFactory;
 use CommerceWeavers\SyliusTpayPlugin\Api\Factory\NextCommand\PayByRedirectFactory;
 use CommerceWeavers\SyliusTpayPlugin\Api\Factory\NextCommandFactory;
 use CommerceWeavers\SyliusTpayPlugin\Api\Factory\NextCommandFactoryInterface;
@@ -25,6 +26,10 @@ return function(ContainerConfigurator $container): void {
     ;
 
     $services->set('commerce_weavers_tpay.api.factory.next_command.pay_by_card', PayByCardFactory::class)
+        ->tag('commerce_weavers_tpay.api.factory.next_command')
+    ;
+
+    $services->set('commerce_weavers_tpay.api.factory.next_command.pay_by_google_pay', PayByGooglePayFactory::class)
         ->tag('commerce_weavers_tpay.api.factory.next_command')
     ;
 

coldic3/CommerceWeaversSyliusTpayPlugin

config/services/api/command.php
@@ -7,6 +7,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 use CommerceWeavers\SyliusTpayPlugin\Api\Command\AbstractPayByHandler;
 use CommerceWeavers\SyliusTpayPlugin\Api\Command\PayByBlikHandler;
 use CommerceWeavers\SyliusTpayPlugin\Api\Command\PayByCardHandler;
+use CommerceWeavers\SyliusTpayPlugin\Api\Command\PayByGooglePayHandler;
 use CommerceWeavers\SyliusTpayPlugin\Api\Command\PayByRedirectHandler;
 use CommerceWeavers\SyliusTpayPlugin\Api\Command\PayHandler;
 
@@ -37,6 +38,11 @@ return function(ContainerConfigurator $container): void {
     ;
 
     $services->set('commerce_weavers_sylius_tpay.api.command.pay_by_card_handler', PayByCardHandler::class)
+        ->parent('commerce_weavers_sylius_tpay.api.command.abstract_pay_by_handler')
+        ->tag('messenger.message_handler')
+    ;
+
+    $services->set('commerce_weavers_sylius_tpay.api.command.pay_by_google_pay_handler', PayByGooglePayHandler::class)
         ->parent('commerce_weavers_sylius_tpay.api.command.abstract_pay_by_handler')
         ->tag('messenger.message_handler')
     ;

config/services/api/factory.php
@@ -6,6 +6,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use CommerceWeavers\SyliusTpayPlugin\Api\Factory\NextCommand\PayByBlikFactory;
 use CommerceWeavers\SyliusTpayPlugin\Api\Factory\NextCommand\PayByCardFactory;
+use CommerceWeavers\SyliusTpayPlugin\Api\Factory\NextCommand\PayByGooglePayFactory;
 use CommerceWeavers\SyliusTpayPlugin\Api\Factory\NextCommand\PayByRedirectFactory;
 use CommerceWeavers\SyliusTpayPlugin\Api\Factory\NextCommandFactory;
 use CommerceWeavers\SyliusTpayPlugin\Api\Factory\NextCommandFactoryInterface;
@@ -25,6 +26,10 @@ return function(ContainerConfigurator $container): void {
     ;
 
     $services->set('commerce_weavers_sylius_tpay.api.factory.next_command.pay_by_card', PayByCardFactory::class)
+        ->tag('commerce_weavers_sylius_tpay.api.factory.next_command')
+    ;
+
+    $services->set('commerce_weavers_sylius_tpay.api.factory.next_command.pay_by_google_pay', PayByGooglePayFactory::class)
         ->tag('commerce_weavers_sylius_tpay.api.factory.next_command')
     ;
 