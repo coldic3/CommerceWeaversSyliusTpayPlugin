d0b85ab9b69355b278bb0ec23c688d164d9444b5
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2024-10-15 14:01:24 +0200

    Add API contract tests for 1-click BLIK

--- tests/Api/Shop/PayingForOrdersByBlikTest.php
+++ tests/Api/Shop/PayingForOrdersByBlikTest.php
@@ -69,6 +69,93 @@ final class PayingForOrdersByBlikTest extends JsonApiTestCase
         );
     }
 
+    public function test_paying_with_a_valid_blik_token_and_saving_alias(): void
+    {
+        $this->loadFixturesFromDirectory('shop/paying_for_orders_by_blik');
+
+        $order = $this->doPlaceOrder('t0k3n', paymentMethodCode: 'tpay_blik');
+
+        $this->client->request(
+            Request::METHOD_POST,
+            sprintf('/api/v2/shop/orders/%s/pay', $order->getTokenValue()),
+            server: self::CONTENT_TYPE_HEADER,
+            content: json_encode([
+                'successUrl' => 'https://example.com/success',
+                'failureUrl' => 'https://example.com/failure',
+                'blikToken' => '777123',
+                'blikSaveAlias' => true,
+            ]),
+        );
+
+        $response = $this->client->getResponse();
+
+        $this->assertResponseCode($response, Response::HTTP_OK);
+        $this->assertResponse($response, 'shop/paying_for_orders_by_blik/test_paying_with_a_valid_blik_token_and_saving_alias');
+    }
+
+    public function test_paying_using_a_valid_blik_alias(): void
+    {
+        $this->loadFixturesFromDirectory('shop/paying_for_orders_by_blik');
+
+        $order = $this->doPlaceOrder('t0k3n', paymentMethodCode: 'tpay_blik');
+
+        $this->client->request(
+            Request::METHOD_POST,
+            sprintf('/api/v2/shop/orders/%s/pay', $order->getTokenValue()),
+            server: self::CONTENT_TYPE_HEADER,
+            content: json_encode([
+                'successUrl' => 'https://example.com/success',
+                'failureUrl' => 'https://example.com/failure',
+                'blikAlias' => 'myuniqalias',
+            ]),
+        );
+
+        $response = $this->client->getResponse();
+
+        $this->assertResponseCode($response, Response::HTTP_OK);
+        $this->assertResponse($response, 'shop/paying_for_orders_by_blik/test_paying_with_a_valid_blik_token_and_saving_alias');
+    }
+
+    public function test_paying_using_a_valid_blik_alias_registered_in_different_banks(): void
+    {
+        $this->loadFixturesFromDirectory('shop/paying_for_orders_by_blik');
+
+        $order = $this->doPlaceOrder('t0k3n', paymentMethodCode: 'tpay_blik');
+
+        $this->client->request(
+            Request::METHOD_POST,
+            sprintf('/api/v2/shop/orders/%s/pay', $order->getTokenValue()),
+            server: self::CONTENT_TYPE_HEADER,
+            content: json_encode([
+                'successUrl' => 'https://example.com/success',
+                'failureUrl' => 'https://example.com/failure',
+                'blikAlias' => 'mynonuniqalias',
+            ]),
+        );
+
+        $response = $this->client->getResponse();
+
+        $this->assertResponseCode($response, Response::HTTP_UNPROCESSABLE_ENTITY);
+        $this->assertResponse($response, 'shop/paying_for_orders_by_blik/test_paying_using_a_valid_blik_alias_registered_in_different_alternatives');
+
+        $this->client->request(
+            Request::METHOD_POST,
+            sprintf('/api/v2/shop/orders/%s/pay', $order->getTokenValue()),
+            server: self::CONTENT_TYPE_HEADER,
+            content: json_encode([
+                'successUrl' => 'https://example.com/success',
+                'failureUrl' => 'https://example.com/failure',
+                'blikAlias' => 'mynonuniqalias',
+                'blikApplicationCode' => '1ec8fe63-ea6e-6b48-ac6f-f7f170888d37',
+            ]),
+        );
+
+        $response = $this->client->getResponse();
+
+        $this->assertResponseCode($response, Response::HTTP_OK);
+        $this->assertResponse($response, 'shop/paying_for_orders_by_blik/test_paying_using_a_valid_blik_alias_registered_in_different_banks');
+    }
+
     public function test_paying_with_a_too_short_blik_token(): void
     {
         $this->loadFixturesFromDirectory('shop/paying_for_orders_by_blik');
