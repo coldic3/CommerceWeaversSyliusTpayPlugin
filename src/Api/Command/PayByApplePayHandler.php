15120348ac5d123ebc95f6993538be96244b3804
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2024-10-16 14:33:46 +0200

    Remove the redundant `PayByApplePayHandler::createResultFrom` method

--- src/Api/Command/PayByApplePayHandler.php
+++ src/Api/Command/PayByApplePayHandler.php
@@ -7,7 +7,6 @@ namespace CommerceWeavers\SyliusTpayPlugin\Api\Command;
 use CommerceWeavers\SyliusTpayPlugin\Model\PaymentDetails;
 use Sylius\Component\Core\Model\PaymentInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
-use Webmozart\Assert\Assert;
 
 #[AsMessageHandler]
 final class PayByApplePayHandler extends AbstractPayByHandler
@@ -28,17 +27,5 @@ final class PayByApplePayHandler extends AbstractPayByHandler
         $paymentDetails->setApplePayToken($applePayToken);
 
         $payment->setDetails($paymentDetails->toArray());
-    }
-
-    private function createResultFrom(PaymentInterface $payment): PayResult
-    {
-        $paymentDetails = PaymentDetails::fromArray($payment->getDetails());
-
-        Assert::notNull($paymentDetails->getStatus(), 'Payment status is required to create a result.');
-
-        return new PayResult(
-            $paymentDetails->getStatus(),
-            $paymentDetails->getPaymentUrl(),
-        );
     }
 }
