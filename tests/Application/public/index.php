81bcd17102554ebe0e07be9fed25e9d88026eac7
Kevin Kaniaburka <kevin.kaniaburka@gmail.com>
2024-11-04 18:35:41 +0100

    Set up Tpay logger in DI extension

--- tests/Application/public/index.php
+++ tests/Application/public/index.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use App\Kernel;
-use Tpay\OpenApi\Utilities\Logger;
 
 $_SERVER['APP_RUNTIME_OPTIONS'] = [
     'project_dir' => dirname(__DIR__),
@@ -12,10 +11,5 @@ $_SERVER['APP_RUNTIME_OPTIONS'] = [
 require_once dirname(__DIR__, 3).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    $kernel = new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-
-    // set up Tpay logger
-    Logger::setLogPath(sprintf('%s/tpay_open_api_', $kernel->getLogDir()));
-
-    return $kernel;
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
