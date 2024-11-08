c88edefcf645c61ed29fdaab64fbe71b560151a7
Kevin Kaniaburka <kevin.kaniaburka@gmail.com>
2024-11-08 09:07:55 +0100

    Resolve tpay transaction channels into an array if an error occurred

--- src/Tpay/Resolver/TpayTransactionChannelResolver.php
+++ src/Tpay/Resolver/TpayTransactionChannelResolver.php
@@ -4,28 +4,57 @@ declare(strict_types=1);
 
 namespace CommerceWeavers\SyliusTpayPlugin\Tpay\Resolver;
 
-use CommerceWeavers\SyliusTpayPlugin\Payum\Exception\UnableToGetBankListException;
+use CommerceWeavers\SyliusTpayPlugin\Payum\Factory\GetTpayTransactionsChannelsFactoryInterface;
 use CommerceWeavers\SyliusTpayPlugin\Payum\Request\Api\GetTpayTransactionsChannels;
 use Payum\Core\Model\ArrayObject;
 use Payum\Core\Payum;
+use Psr\Log\LoggerInterface;
+use Tpay\OpenApi\Utilities\TpayException;
 
 final class TpayTransactionChannelResolver implements TpayTransactionChannelResolverInterface
 {
     public function __construct(
         private readonly Payum $payum,
+        private readonly ?GetTpayTransactionsChannelsFactoryInterface $getTpayTransactionsChannelsFactory = null,
+        private readonly ?LoggerInterface $logger = null,
     ) {
+        if (null === $this->getTpayTransactionsChannelsFactory) {
+            trigger_deprecation(
+                'commerce-weavers/sylius-tpay-plugin',
+                '1.0',
+                'Not passing a $getTpayTransactionsChannelsFactory to %s constructor is deprecated and will be removed in SyliusTpayPlugin 2.0.',
+                self::class,
+            );
+        }
     }
 
     public function resolve(): array
     {
         $gateway = $this->payum->getGateway('tpay');
 
-        $gateway->execute($value = new GetTpayTransactionsChannels(new ArrayObject()), true);
+        $value = $this->getTpayTransactionsChannelsFactory?->createNewEmpty()
+            ?? new GetTpayTransactionsChannels(new ArrayObject());
+
+        try {
+            $gateway->execute($value, true);
+        } catch (TpayException $e) {
+            $this->logger?->critical('Unable to get banks list. TpayException thrown.', ['exceptionMessage' => $e->getMessage()]);
+
+            return [];
+        }
 
         $result = $value->getResult();
 
         if (!isset($result['result']) || 'success' !== $result['result']) {
-            throw new UnableToGetBankListException('Unable to get banks list. Response: ' . json_encode($result));
+            $this->logger?->critical('Unable to get banks list. The result is not success.', ['responseBody' => json_encode($result)]);
+
+            return [];
+        }
+
+        if (!isset($result['channels'])) {
+            $this->logger?->critical('Unable to get banks list. The channels key is missing.', ['responseBody' => json_encode($result)]);
+
+            return [];
         }
 
         $indexedResult = [];
