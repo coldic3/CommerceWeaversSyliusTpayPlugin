93b63983eb24fda5b5ef520ae63b2cd7e7d375a0
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2024-11-28 18:13:42 +0100

    Fix not being able to select `Display all channels` with PBL payment method once a connection is checked

--- src/PayByLinkPayment/Form/Type/GatewayConfigurationType.php
+++ src/PayByLinkPayment/Form/Type/GatewayConfigurationType.php
@@ -4,12 +4,23 @@ declare(strict_types=1);
 
 namespace CommerceWeavers\SyliusTpayPlugin\PayByLinkPayment\Form\Type;
 
+use CommerceWeavers\SyliusTpayPlugin\Form\EventListener\DecryptGatewayConfigListenerInterface;
+use CommerceWeavers\SyliusTpayPlugin\Form\EventListener\EncryptGatewayConfigListenerInterface;
 use CommerceWeavers\SyliusTpayPlugin\Form\Type\AbstractTpayGatewayConfigurationType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class GatewayConfigurationType extends AbstractTpayGatewayConfigurationType
 {
+    public function __construct(
+        DecryptGatewayConfigListenerInterface $decryptGatewayConfigListener,
+        EncryptGatewayConfigListenerInterface $encryptGatewayConfigListener,
+        private readonly TranslatorInterface $translator,
+    ) {
+        parent::__construct($decryptGatewayConfigListener, $encryptGatewayConfigListener);
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         parent::buildForm($builder, $options);
@@ -23,6 +34,10 @@ final class GatewayConfigurationType extends AbstractTpayGatewayConfigurationTyp
                     'label' => 'commerce_weavers_sylius_tpay.admin.gateway_configuration.tpay_channel_id',
                     'help' => 'commerce_weavers_sylius_tpay.admin.gateway_configuration.tpay_channel_id_help',
                     'required' => false,
+                    'attr' => [
+                        'placeholder' => $this->translator->trans('commerce_weavers_sylius_tpay.admin.gateway_configuration.tpay_display_all_channels', domain: 'messages'),
+                        'data-display-all-label' => $this->translator->trans('commerce_weavers_sylius_tpay.admin.gateway_configuration.tpay_display_all_channels', domain: 'messages'),
+                    ],
                 ],
             )
         ;
