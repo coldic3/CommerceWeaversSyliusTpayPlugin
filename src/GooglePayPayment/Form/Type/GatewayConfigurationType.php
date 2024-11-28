df94ef71705506261c7a4586629e4b5116a4c990
Łukasz Chruściel <lchrusciel@gmail.com>
2024-11-28 23:29:56 +0100

    [Apple][Google] Add requirement to define apple and google identifiers

--- src/GooglePayPayment/Form/Type/GatewayConfigurationType.php
+++ src/GooglePayPayment/Form/Type/GatewayConfigurationType.php
@@ -7,6 +7,7 @@ namespace CommerceWeavers\SyliusTpayPlugin\GooglePayPayment\Form\Type;
 use CommerceWeavers\SyliusTpayPlugin\Form\Type\AbstractTpayGatewayConfigurationType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 final class GatewayConfigurationType extends AbstractTpayGatewayConfigurationType
 {
@@ -23,6 +24,10 @@ final class GatewayConfigurationType extends AbstractTpayGatewayConfigurationTyp
                     'label' => 'commerce_weavers_sylius_tpay.admin.gateway_configuration.google_merchant_id',
                     'required' => true,
                     'priority' => -250,
+                    'constraints' => [
+                        new NotBlank(allowNull: false, groups: ['sylius']),
+                    ],
+                    'validation_groups' => ['sylius'],
                 ],
             )
         ;
